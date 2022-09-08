6e3d2e63d4989ff84389ec31bfd03aee7b24d0d9
BeshoyHani <be4oy.hani@gmail.com>
2022-09-08 20:07:56 +0200

    fix small width issue

--- src/components/URL/URLImg.js
+++ src/components/URL/URLImg.js
@@ -8,8 +8,8 @@ export const URLImg = ({ imgURL, alt }) => {
                 margin: 2,
                 height: '300',
                 width: '500',
-                maxHeight: { xs: 300, md: 250 },
-                maxWidth: { xs: 200, md: 400 },
+                maxHeight: { xs: 200, md: 250 },
+                maxWidth: { xs: 100, md: 400 },
             }}
             alt={alt}
             src={imgURL || 'https://res.cloudinary.com/dpecxlfg2/image/upload/v1662063078/urls_preview/images_rvmnod.png'}
